6d9ec84438b86b96ee07d813c95276a4b3a5ffab
RalfiSlask <nilssonnarki@gmail.com>
2023-10-21 13:22:10 +0200

    22th oc

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 "use strict";
 const slider = document.querySelector("input");
 const operators = document.querySelectorAll(".operator");
-const numbers = document.querySelectorAll(".numbers");
+const numbers = document.querySelectorAll(".number");
 const display = document.querySelector(".display_container");
 const body = document.querySelector("body");
 slider === null || slider === void 0 ? void 0 : slider.addEventListener("change", () => {
@@ -14,4 +14,37 @@ slider === null || slider === void 0 ? void 0 : slider.addEventListener("change"
             body === null || body === void 0 ? void 0 : body.classList.add("T3");
         }
     }
+});
+let displayArray = ["0"];
+const lastIndex = displayArray.length;
+operators.forEach(operator => {
+    let isLastIndexAnOperator;
+    if (operator.textContent) {
+        isLastIndexAnOperator = displayArray[lastIndex - 1].includes(operator.textContent);
+        console.log(isLastIndexAnOperator);
+    }
+    operator.addEventListener("click", () => {
+        if ((display === null || display === void 0 ? void 0 : display.textContent) && operator.textContent) {
+            if (isLastIndexAnOperator) {
+                displayArray[lastIndex - 1] = operator.textContent;
+            }
+            else {
+                displayArray.push(operator.textContent);
+            }
+            display.textContent = displayArray.join("");
+        }
+    });
+});
+numbers.forEach(number => {
+    number.addEventListener("click", () => {
+        if ((display === null || display === void 0 ? void 0 : display.textContent) && number.textContent) {
+            if (displayArray[0] === "0") {
+                displayArray[0] = number.textContent;
+            }
+            else {
+                displayArray.push(number.textContent);
+            }
+            display.textContent = displayArray.join("");
+        }
+    });
 });
--- script.ts
+++ script.ts
@@ -1,8 +1,9 @@
 const slider = document.querySelector("input");
 const operators = document.querySelectorAll(".operator");
-const numbers = document.querySelectorAll(".numbers");
+const numbers = document.querySelectorAll(".number");
 const display = document.querySelector(".display_container");
 const body = document.querySelector("body");
+
 
 slider?.addEventListener("change", () => {
     if(body) {
@@ -15,5 +16,36 @@ slider?.addEventListener("change", () => {
     }
 });
 
+let displayArray: string[] = ["0"];
+const lastIndex = displayArray.length;
 
+operators.forEach(operator => {
+    let isLastIndexAnOperator: boolean;
+    if(operator.textContent) {
+        isLastIndexAnOperator = displayArray[lastIndex - 1].includes(operator.textContent)
+        console.log(isLastIndexAnOperator)
+    }
+    operator.addEventListener("click", () => {
+        if(display?.textContent && operator.textContent) {
+            if(isLastIndexAnOperator) {
+                displayArray[lastIndex - 1] = operator.textContent;
+            } else {
+                displayArray.push(operator.textContent)
+            }
+            display.textContent = displayArray.join("");
+        }
+    })
+});
 
+numbers.forEach(number => {
+    number.addEventListener("click", () => {
+        if(display?.textContent && number.textContent) {
+            if(displayArray[0] === "0") {
+                displayArray[0] = number.textContent;
+            } else {
+                displayArray.push(number.textContent);
+            }
+            display.textContent = displayArray.join("");
+        }   
+    })
+});
